14be7d248cedbb518260e26a8e54865535614661
mohamed gamale <mohamedgamalmohamed48@gmail.comm>
2019-10-30 23:36:48 +0200

    first commit

---
+++ Animate cardview/CardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CardViewController.swift
+//  Animate cardview
+//
+//  Created by mohamed gamal on 10/30/19.
+//  Copyright © 2019 mohamed gamal. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
